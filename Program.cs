961d6486e3424530be50bfac5560c4673bcdb4b0
Uncle.Chao <uncle.chao@live.cn>
2014-08-02 23:12:37 +0800

    Thread Control Class

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Bank crashBank = Bank.GetInstance();
+            #region single thread
+            //crashBank.SaveMoney(r.Next(1, 1000));
+            //crashBank.SaveMoney(r.Next(1, 1000));
+            //crashBank.DrawMoney(r.Next(1, 1000));
+            //crashBank.SaveMoney(r.Next(1, 1000));
+            //crashBank.DrawMoney(r.Next(1, 1000));
+            //crashBank.DrawMoney(r.Next(1, 1000));
+            //crashBank.SaveMoney(r.Next(1, 1000));
+            //crashBank.DrawMoney(r.Next(1, 1000));
+            //crashBank.DrawMoney(r.Next(1, 1000));
+            #endregion
+            #region Multithreading
+            Thread t1 = new Thread(crashBank.SaveMoney);
+            Thread t2 = new Thread(crashBank.DrawMoney);
+            Thread t3 = new Thread(crashBank.SaveMoney);
+            Thread t4 = new Thread(crashBank.DrawMoney);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
