ca7d8eaf75441a07b8808d0f33a8e7efb53548b0
Uncle.Chao <uncle.chao@live.cn>
2014-08-02 23:12:22 +0800

    Bank Class

---
+++ Bank.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public sealed class Bank
+    {
+        private Random r = new Random();
+        private static int balance = 0;
+        private static readonly Bank p_bank = new Bank();
+        private Bank() { }
+        public static Bank GetInstance()
+        {
+            return p_bank;
+        }
+
+        public void SaveMoney()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    int amount = r.Next(1, 10);
+                    balance += amount;
+                    Console.WriteLine("Save:{0},balance:{1}", amount, Bank.balance);
+                }
+            }
+        }
+
+        public void DrawMoney()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    int amount = r.Next(1, 10);
+                    if ((balance - amount) >= 0)
+                    {
+                        balance -= amount; ;
+                        Console.WriteLine("Draw:{0},balance:{1}", amount, Bank.balance);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Want to Draw:{0},Not enough money! Balance:{1}", amount, Bank.balance);
+                    }
+                }
+            }
+        }
+    }
+}
